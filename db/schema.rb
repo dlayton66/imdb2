655212a858346c0065d54ccd2828c07419261961
Drew Layton <dlayton66@gmail.com>
2022-12-01 14:29:28 -0700

    Generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_211328) do
+ActiveRecord::Schema.define(version: 2022_12_01_212512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,4 +24,18 @@ ActiveRecord::Schema.define(version: 2022_12_01_211328) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "movies", force: :cascade do |t|
+    t.bigint "director_id"
+    t.string "title"
+    t.integer "runtime"
+    t.date "release"
+    t.boolean "like"
+    t.integer "dmst_gross"
+    t.integer "intl_gross"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["director_id"], name: "index_movies_on_director_id"
+  end
+
+  add_foreign_key "movies", "directors"
 end
