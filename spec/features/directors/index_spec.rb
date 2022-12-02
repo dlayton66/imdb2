f730dd2f806b92d3b81acaccbcc3d485c897ea20
Drew Layton <dlayton66@gmail.com>
2022-12-01 18:06:10 -0700

    test: '/directors' lists directors

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -1,13 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Directors Index' do
+RSpec.describe "Directors Index" do
 
-  describe 'User Story 1' do
-    describe "User visits '/parents'" do
-      it 'shows the name of every parent record' do
-        director_1 = Director.create(firstname: "Stanley",
-                                     surname: "Kubrick",
-                                     )
+  describe "User Story 1" do
+    describe "User visits '/directors'" do
+      it "shows the name of every director" do
+        director_1 = Director.create!(firstname: "Stanley",
+                                     lastname: "Kubrick",
+                                     alive: false,
+                                     age: 70)
+        director_2 = Director.create!(firstname: "Paul Thomas",
+                                     lastname: "Anderson",
+                                     alive: true,
+                                     age: 52)
+
+        visit "/directors"
+
+        expect(page).to have_content("Stanley Kubrick")
+        expect(page).to have_content("Paul Thomas Anderson")
       end
     end
   end
