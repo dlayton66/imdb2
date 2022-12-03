9fcacdb0086ab27e61643dc9dd3d09e9e3f69acd
Drew Layton <dlayton66@gmail.com>
2022-12-02 21:59:30 -0700

    test: Show and order by created_at on '/directors'

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -3,15 +3,25 @@ require "rails_helper"
 RSpec.describe "Directors Index" do
 
   before :each do
-    director_1 = Director.create!(firstname: "Stanley",
-      lastname: "Kubrick",
-      alive: false,
-      age: 70)
+    @director_1 = Director.create!(firstname: "Stanley",
+                                  lastname: "Kubrick",
+                                  alive: false,
+                                  age: 70)
       
-    director_2 = Director.create!(firstname: "Paul Thomas",
-      lastname: "Anderson",
-      alive: true,
-      age: 52)
+    @director_2 = Director.create!(firstname: "Paul Thomas",
+                                  lastname: "Anderson",
+                                  alive: true,
+                                  age: 52)
+                                  
+    @director_3 = Director.create!(firstname: "Ed",
+                                  lastname: "Wood",
+                                  alive: false,
+                                  age: 54)
+
+    @director_4 = Director.create!(firstname: "Jim",
+                                  lastname: "Jarmusch",
+                                  alive: true,
+                                  age: 69)
   end
 
   describe "User Story 1" do
@@ -21,6 +31,32 @@ RSpec.describe "Directors Index" do
 
         expect(page).to have_content("Stanley Kubrick")
         expect(page).to have_content("Paul Thomas Anderson")
+      end
+    end
+  end
+
+  describe "User Story 6" do
+    describe "User visits '/directors'" do
+      it "orders directors by most recently created" do
+        visit "/directors"
+
+        kubrick = page.body.index(@director_1.fullname)
+        pta = page.body.index(@director_2.fullname)
+        wood = page.body.index(@director_3.fullname)
+        jarmusch = page.body.index(@director_4.fullname)
+
+        expect(kubrick).to be < pta
+        expect(pta).to be < wood
+        expect(wood).to be < jarmusch
+      end
+
+      it "shows when each record was created" do
+        visit "/directors"
+
+        expect(page).to have_content(@director_1.created_at)
+        expect(page).to have_content(@director_2.created_at)
+        expect(page).to have_content(@director_3.created_at)
+        expect(page).to have_content(@director_4.created_at)
       end
     end
   end
--- spec/features/directors/show_spec.rb
+++ spec/features/directors/show_spec.rb
@@ -3,12 +3,12 @@ require "rails_helper"
 RSpec.describe "Directors Index" do
 
   before :each do
-    director_1 = Director.create!(firstname: "Stanley",
+    @director_1 = Director.create!(firstname: "Stanley",
       lastname: "Kubrick",
       alive: false,
       age: 70)
       
-    director_2 = Director.create!(firstname: "Paul Thomas",
+    @director_2 = Director.create!(firstname: "Paul Thomas",
       lastname: "Anderson",
       alive: true,
       age: 52)
@@ -17,12 +17,13 @@ RSpec.describe "Directors Index" do
   describe "User Story 2" do
     describe "User visits '/directors/:id'" do
       it "shows the attributes for that director" do
-        director_id = Director.find_by(firstname: "Stanley", lastname: "Kubrick").id
+        director_id = Director.find_by(firstname: @director_1.firstname, 
+                                       lastname: @director_1.lastname).id
         visit "/directors/#{director_id}"
 
-        expect(page).to have_content("Stanley Kubrick")
-        expect(page).to have_content("Alive: false")
-        expect(page).to have_content("Age: 70")
+        expect(page).to have_content(@director_1.fullname)
+        expect(page).to have_content("Alive: #{@director_1.alive}")
+        expect(page).to have_content("Age: #{@director_1.age}")
       end
     end
   end
