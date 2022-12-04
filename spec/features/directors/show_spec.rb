c1e39e9536c0793ca1e4bd1c9653405f0a49601d
Drew Layton <dlayton66@gmail.com>
2022-12-04 09:58:01 -0700

    feat/test: Add movies index link for individual directors

--- spec/features/directors/show_spec.rb
+++ spec/features/directors/show_spec.rb
@@ -31,9 +31,12 @@ RSpec.describe "Directors Index" do
   describe "User Story 2" do
     describe "User visits '/directors/:id'" do
       it "shows the attributes for that director" do
-        director_id = Director.find_by(firstname: @director_1.firstname, 
-                                       lastname: @director_1.lastname).id
-        visit "/directors/#{director_id}"
+        visit "/directors/#{
+                            Director.find_by(
+                                             firstname: @director_1.firstname, 
+                                             lastname: @director_1.lastname
+                                            ).id
+                           }"
 
         expect(page).to have_content(@director_1.fullname)
         expect(page).to have_content("Alive: #{@director_1.alive}")
@@ -43,13 +46,31 @@ RSpec.describe "Directors Index" do
   end
 
   describe "User Story 7" do
-    describe "User visits'/directors/:id'" do
+    describe "User visits '/directors/:id'" do
       it "shows how many movie records that director has" do
         director_id = Director.find_by(firstname: @director_1.firstname, 
                                        lastname: @director_1.lastname).id
         visit "/directors/#{director_id}"
 
         expect(page).to have_content("Number of movies: #{@director_1.movies.count}")
+      end
+    end
+  end
+
+  describe "User Story 10" do
+    describe "User visits '/directors/:id'" do
+      it "has a link to that director's movies index page" do
+        director_id = Director.find_by(firstname: @director_1.firstname, 
+                                       lastname: @director_1.lastname).id
+        visit "/directors/#{director_id}"
+
+        expect(page).to have_link(href: "/directors/#{director_id}/movies")
+
+        director_id = Director.find_by(firstname: @director_2.firstname, 
+          lastname: @director_2.lastname).id
+        visit "/directors/#{director_id}"
+
+        expect(page).to have_link(href: "/directors/#{director_id}/movies")
       end
     end
   end
