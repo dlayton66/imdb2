10032ce6b5271a8ec023b24f2712155355a05fa0
Drew Layton <dlayton66@gmail.com>
2022-12-04 09:21:40 -0700

    test: Visit another page to test movies index

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Navigation Bar" do
-  # Build this out more?
+  # Build these out more?
   describe "User Story 8" do
     it "has a link to movies index at the top of every page" do
-      visit "/directors"
+      visit '/directors'
+      expect(page).to have_link('Movies', href: '/movies')
 
+      visit '/movies'
       expect(page).to have_link('Movies', href: '/movies')
     end
   end
+
+  describe "User Story 9"
 end
