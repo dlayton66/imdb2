74d3d2fc40f4d734a7f4c1104f3dda82a8a940bb
Drew Layton <dlayton66@gmail.com>
2022-12-04 09:28:07 -0700

    feat/test: Add link to directors index to every page

--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Navigation Bar" do
-  # Build these out more?
+
+  ########## Build these out more? ##########
+  # Could also test top of page condition
+
   describe "User Story 8" do
     it "has a link to movies index at the top of every page" do
       visit '/directors'
@@ -12,5 +15,13 @@ RSpec.describe "Navigation Bar" do
     end
   end
 
-  describe "User Story 9"
+  describe "User Story 9" do
+    it "has a link to directors index at the top of every page" do
+      visit '/directors'
+      expect(page).to have_link('Directors', href: '/directors')
+
+      visit '/movies'
+      expect(page).to have_link('Directors', href: '/directors')
+    end
+  end
 end
