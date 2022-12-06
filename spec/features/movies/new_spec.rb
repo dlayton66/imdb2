03ce0581ae8bb53d91eb4d7993f493720b1c8aaf
Drew Layton <dlayton66@gmail.com>
2022-12-05 20:27:09 -0700

    fix: Remove commented out user stories

--- spec/features/movies/new_spec.rb
+++ spec/features/movies/new_spec.rb
@@ -1,18 +1,5 @@
 require 'rails_helper'
 
-# User Story 13, Parent Child Creation 
-
-# As a visitor
-# When I visit a Parent Children Index page
-# Then I see a link to add a new adoptable child for that parent "Create Child"
-# When I click the link
-# I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-# When I fill in the form with the child's attributes:
-# And I click the button "Create Child"
-# Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-# a new child object/row is created for that parent,
-# and I am redirected to the Parent Childs Index page where I can see the new child listed
-
 RSpec.describe "Create Movie" do
   before :each do
     seed_kubrick
@@ -41,7 +28,7 @@ RSpec.describe "Create Movie" do
       expect(current_path).to eq("/directors/#{@director_1.id}/movies")
 
       visit("/directors/#{@director_1.id}/movies")
-      
+
       expect(page).to have_content("The Shining")
     end
   end
